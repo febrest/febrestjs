8b335fb8dd67ae22321adf4535ad9f2e6e4b860d
Spencer <pallyoung@163.com>
2018-05-16 22:04:07 +0800

    runtimeaction

---
+++ src/action/runtimeAction.js
@@ -0,0 +1,13 @@
+let runtimeAction;
+
+function setRuntimeAction(action){
+    runtimeAction = action;
+}
+function getRuntimeAction(){
+    return runtimeAction;
+}
+
+export {
+    setRuntimeAction,
+    getRuntimeAction
+}
