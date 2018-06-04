92c12bfd0b8d3502c65e99dd9f518101a6637028
Spencer <pallyoung@163.com>
2018-06-04 14:42:03 +0800

    Lock

--- src/provider/Provider.js
+++ src/provider/Provider.js
@@ -8,6 +8,31 @@ class Provider {
     constructor(config) {
         this.state = config.state;
         this.name = config.name;
+        this._isLock = false;
+        this._lockPromise;
+        this._lockResolve
+    }
+    lock(){
+        if(this._isLock){
+            return;
+        }
+        this._isLock = true;
+        this._lockPromise = new Promise((resolve)=>{
+            this._lockResolve = resolve;
+        });
+    }
+    resolvelock(){
+        this._lockResolve();
+        this._isLock = false;
+        this._lockResolve = undefined;
+        this._lockPromise = undefined;
+    }
+    checkLock(){
+        if(!this._isLock){
+            return Promise.resolve();
+        }else{
+            return this._lockPromise
+        }
     }
     getState() {
         return copy(this.state);
