1dd1fe1f8fe38fb0ced762be71b10354ca92ffd1
Spencer <pallyoung@163.com>
2018-07-04 17:12:51 +0800

    彻底移除不用的provider

--- src/provider/index.js
+++ src/provider/index.js
@@ -1,16 +1,12 @@
 'use strict'
 import Provider from './Provider';
 import ProviderContainer from './ProviderContainer';
-// import RemoteProvider from './RemoteProvider';
 import SessionProvider from './SessionProvider';
-// import StorageProvider from './StorageProvider';
 import ProviderCreator from './ProviderCreator';
 import inject from './inject';
 
 const use = ProviderCreator.use;
 
-// use('storage',StorageProvider);
-// use('remote',RemoteProvider);
 use('session',SessionProvider);
 
 
