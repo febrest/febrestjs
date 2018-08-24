430f0522a624ac317c2be6df9f9c76c7914647e6
pallyoung <pallyoung@163.com>
2018-08-24 18:41:23 +0800

    providerfactory

--- src/provider/ProviderFactory.js
+++ src/provider/ProviderFactory.js
@@ -2,14 +2,7 @@
 import ProviderWrapper from './ProviderWrapper';
 function ProviderFactory(config) {
     let provider = new ProviderWrapper(config);
-    return {
-        fetch:function(action,payload){
-            return provider.fetch(action,payload);
-        },
-        update:function(){
-            return provider.update(action,payload);
-        }
-    }
+    return provider;
 }
 
 export default ProviderFactory;
