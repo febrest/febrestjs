80390f80e8f37521d76c057a251d1d67b85ef32b
pallyoung <pallyoung@163.com>
2018-08-24 18:39:59 +0800

    provideradapter

--- src/provider/ProviderAdapter.js
+++ src/provider/ProviderAdapter.js
@@ -1,10 +1,10 @@
 'use strict'
 class ProviderAdapter {
-    fetch(provider, action, payload) {
-        return provider.fetch
+    fetch(origin, action, payload) {
+        return origin.fetch(action, payload);
     }
-    update(provider, action, payload) {
-        return provider.update(action, payload);
+    update(origin, action, payload) {
+        return origin.update(action, payload);
     }
     onError({ origin, action, payload, error, type }) {
 
