fbcbb7567035e5526cd5bd13cc24a209e2cef938
pallyoung <pallyoung@163.com>
2018-08-24 14:29:22 +0800

    actionflow移除

--- src/action/index.js
+++ src/action/index.js
@@ -1,16 +1,13 @@
 import ActionRegister from './ActionRegister';
-import {initialize,exception,exec,close} from './actionFlow'
 import ACTION_READY_STATE from './ACTION_READY_STATE';
-import {getRuntimeAction,setRuntimeAction} from './runtimeAction';
+import { getRuntimeAction, setRuntimeAction, clearRuntimeAction, createRuntimeAction } from './runtimeAction';
 
 
 export {
     ActionRegister,
-    initialize,
-    exec,
-    close,
-    exception,
     ACTION_READY_STATE,
     getRuntimeAction,
-    setRuntimeAction
+    setRuntimeAction,
+    clearRuntimeAction,
+    createRuntimeAction
 };
