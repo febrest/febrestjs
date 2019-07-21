22099d445eb2826821f17a313904e66bb8d4a014
Spencer <pallyoung@163.com>
2019-07-21 16:38:24 +0800

    refactor:move bordercast to  bordercast from observer

---
+++ src/bordercast/index.js
@@ -0,0 +1,5 @@
+import Bordercast from '../Bordercast';
+
+export {
+  Bordercast
+}
--- src/observer/index.js
+++ src/observer/index.js
@@ -1,9 +1,7 @@
-import Bordercast from './Bordercast';
 import {watch, doWatch, pendingWatch, unwatch} from './Observer';
 
 
 export  {
-    Bordercast,
     doWatch,
     watch,
     unwatch,
