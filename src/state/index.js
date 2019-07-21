6a38198c5e94d1b3b592c64c88fb346f1a87f2aa
Spencer <pallyoung@163.com>
2019-07-21 16:45:32 +0800

    refactor:remove services

--- src/state/index.js
+++ src/state/index.js
@@ -1,11 +1,7 @@
 'use strict'
 
 import State from './State';
-import StateRegister from './StateRegister'
-import StateAdapter from './StateAdapter'
 
 export {
     State,
-    StateRegister,
-    StateAdapter
 }
