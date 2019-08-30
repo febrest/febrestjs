4023703e7f22ffce5b601751260789469de3f911
Spencer <pallyoung@163.com>
2019-08-30 22:42:02 +0800

    state重写

--- src/state/State.js
+++ src/state/State.js
@@ -1,28 +1,39 @@
-import { copy } from '../util';
-
+import { copy } from "../util";
+const STATE_MAP = new Map();
 class State {
-    constructor(data) {
-        this.data = data !== undefined ? copy(data) : { };
-        this.$typeof$ = 'State';
+  constructor() {
+    this.data = null;
+  }
+  set(data) {
+    this.data = copy(data);
+  }
+  get() {
+    return copy(this.data);
+  }
+  toString() {
+    return JSON.stringify(this.data);
+  }
+  parse(string) {
+    this.data = JSON.parse(string);
+  }
+}
+function StateFactory(name) {
+  const state = new State();
+  const stateWrapper = {
+    $type$: "State",
+    get: function() {
+      return state.get();
+    },
+    set: function(data) {
+      return state.set(data);
     }
-    onCreate() {
-
-    }
-    onDestory() {
-        this.data = null;
-    }
-    set(data) {
-        this.data = copy(data);
-    }
-    get() {
-        return copy(this.data);
-    }
-    toString() {
-        return JSON.stringify(this.data);
-    }
-    parse(string) {
-        this.data = JSON.parse(string)
-    }
+  };
+  STATE_MAP.set(name, stateWrapper);
+  return stateWrapper;
+}
+function getOrCreateState(name) {
+  const state = STATE_MAP.get(name) || StateFactory(name);
+  return state;
 }
 
-export default State;
+export default getOrCreateState;
