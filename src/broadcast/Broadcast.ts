cbe576247d3e0c4a58a69f8c96f4d0f18a6828da
pallyoung <pallyoung@163.com>
2019-11-26 16:46:44 +0800

    refactor:Broadcast重构

--- src/broadcast/Broadcast.ts
+++ src/broadcast/Broadcast.ts
@@ -1,29 +1,33 @@
 "use strict";
 
+export interface BroadcastEvent {
+  cmd: string;
+  data: any;
+}
+export type BroadcastEventListener = (event: BroadcastEvent) => void;
 class Broadcast {
-  constructor() {
-    this._listeners = [];
-  }
-  _message(cmd, data) {
+  private listeners: BroadcastEventListener[] = [];
+
+  private dispatchMessage(cmd: string, data: any) {
     let event = {
       cmd,
       data
     };
-    this._listeners.forEach(callback => {
+    this.listeners.forEach(callback => {
       callback(event);
     });
   }
-  message(cmd, data) {
-    this._message(cmd, data);
+  message(cmd: string, data: any) {
+    this.dispatchMessage(cmd, data);
   }
-  subscribe(callback) {
-    this._listeners.push(callback);
+  subscribe(callback: BroadcastEventListener) {
+    this.listeners.push(callback);
   }
   release() {
-    this._listeners = null;
+    this.listeners = [];
   }
-  unsubscribe(callback) {
-    let listeners = this._listeners;
+  unsubscribe(callback: BroadcastEventListener) {
+    let listeners = this.listeners;
     for (let i = listeners.length - 1; i >= 0; i--) {
       if (listeners[i] === callback) {
         listeners.splice(i, 1);
