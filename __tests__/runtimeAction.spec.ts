f6f12aa0d52a682a2bb29379bf274e0d8c2f0c42
pallyoung <pallyoung@163.com>
2019-11-27 18:38:35 +0800

    test:runtimeAction单元测试

---
+++ __tests__/runtimeAction.spec.ts
@@ -0,0 +1,23 @@
+import {
+  clearRuntimeAction,
+  createRuntimeAction,
+  getRuntimeAction,
+  setRuntimeAction
+} from "action";
+
+import { RuntimeAction } from "action/runtimeAction";
+
+let action: RuntimeAction | undefined;
+test("runtime action", () => {
+  const fn = jest.fn(x => x);
+  action = createRuntimeAction("test", fn, 5);
+  setRuntimeAction(action);
+  expect(action.$typeof$).toBe("RuntimeAction");
+  expect(action.controller).toBe(fn);
+  expect(action.payload).toBe(5);
+  expect(action.name).toBe("test");
+  expect(getRuntimeAction()).toBe(action);
+  clearRuntimeAction(action);
+  expect(action.controller).toBeUndefined();
+  expect(action.payload).toBeUndefined();
+});
