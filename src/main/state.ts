c3c4ac9acfcf91a74d35931f7ae6933cb684340a
pallyoung <pallyoung@163.com>
2019-11-27 15:35:32 +0800

    refactor:main/state重构

--- src/main/state.ts
+++ src/main/state.ts
@@ -1,16 +1,17 @@
 import { batch, state as s, setObserver } from "./../state";
 
-import Observer from "./../observer";
+import Observer from "observer";
+import { ObserverListener } from "observer/Observer";
 
 const observer = new Observer();
 
 setObserver(observer);
 
-const state = function (name) {
+const state = function(name: string) {
   return s(name);
 };
 
-state.observe = function (l) {
+state.observe = function(l: ObserverListener) {
   return observer.observe(l);
 };
 state.batch = batch;
