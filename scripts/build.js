20bc402056a142212d8771950cea5d481a92b207
pallyoung <pallyoung@163.com>
2019-11-28 14:01:25 +0800

    chore:构建工具修改，添加ts支持

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,17 @@
-const rollup = require('./rollup');
+const rollup = require("./rollup");
 
-const { version } = require('./../package');
+const { version } = require("./../package");
 
-const buildTypes = ['dev','prod'];
+const buildTypes = ["dev", "prod"];
 
-const entry = 'src/index.js'
-const name = 'Febrest';
-const output = 'dist'
-async function build(){
-    await rollup(entry,output,name,version,'dev');
-    await rollup(entry,output,name,version,'prod');
+const entry = "src/index.ts";
+const name = "Febrest";
+const output = "dist";
+async function build() {
+  await rollup(entry, output, name, version, "dev");
+  await rollup(entry, output, name, version, "prod");
 }
-if(process.argv[2]){
-    build();
+if (process.argv[2]) {
+  build();
 }
 module.exports = build;
--- scripts/rollup.js
+++ scripts/rollup.js
@@ -1,61 +1,86 @@
-const rollup = require('rollup');
-const babel = require('rollup-plugin-babel');
-const commonjs = require('rollup-plugin-commonjs');
-const uglify = require('rollup-plugin-uglify');
-const prettier = require('rollup-plugin-prettier');
-const path = require('path');
-const fs = require('fs');
+const rollup = require("rollup");
+const babel = require("rollup-plugin-babel");
+const commonjs = require("rollup-plugin-commonjs");
+const { uglify } = require("rollup-plugin-uglify");
+const prettier = require("rollup-plugin-prettier");
+const path = require("path");
+const fs = require("fs");
+const typescript = require("rollup-plugin-typescript2");
 
-const getBanner = require('./banner')
+const alias = require("rollup-plugin-alias");
 
-
+const getBanner = require("./banner");
 
 function getFileName(name, bundleType) {
-    switch (bundleType) {
-        case 'dev':
-            return `${name.toLowerCase()}.js`;
-        case 'prod':
-            return `${name.toLowerCase()}.min.js`;
-    }
+  switch (bundleType) {
+    case "dev":
+      return `${name.toLowerCase()}.js`;
+    case "prod":
+      return `${name.toLowerCase()}.min.js`;
+  }
 }
 
 function getPlugins(bundleType) {
-    return [
-        babel({ runtimeHelpers: true }),
-        commonjs(),
-        bundleType === 'prod' ? uglify() : prettier
-    ]
+  const root = path.resolve("./src");
+  return [
+    alias({
+      resolve: [".ts"],
+      entries: fs.readdirSync(root).map(name => {
+        const stat = fs.statSync(path.resolve(root, name));
+        if (stat.isDirectory()) {
+          return {
+            find: name,
+            replacement: path.resolve(root, name)
+          };
+        }
+        return { find: "index", replacement: path.resolve(root, name) };
+      })
+    }),
+    commonjs(),
+    typescript(),
+    bundleType === "prod"
+      ? uglify()
+      : prettier({
+          tabWidth: 2,
+          singleQuote: false,
+          babel: "babel"
+        })
+  ];
 }
 function getOutputOptions(output, name, bundleType, exts) {
-    var format = 'umd';
-    var file = output;
-    return {
-        file,
-        format,
-        name,
-        ...exts
-    }
+  var format = "umd";
+  var file = output;
+  return {
+    file,
+    format,
+    name,
+    ...exts
+  };
 }
 
-function intro(version){
-    return 'var VERSION = \''+ version+'\'';
+function intro(version) {
+  return "var VERSION = '" + version + "'";
 }
-
 
 async function build(entry, output, name, version, bundleType) {
-    var result = await rollup.rollup({
-        input: entry,
-        plugins:getPlugins(bundleType)
-    });
-    var filename = getFileName(name, bundleType);
-    output = path.resolve(output, filename);
-    var banner = '';
-    if (bundleType === 'dev') {
-        banner = getBanner(version,filename);
-    }
-    if(fs.existsSync(output)) {
-        fs.unlinkSync(output);
-    }
-    await result.write(getOutputOptions(output,name,bundleType,{banner,intro:intro(version)}));
+  var result = await rollup.rollup({
+    input: entry,
+    plugins: getPlugins(bundleType)
+  });
+  var filename = getFileName(name, bundleType);
+  output = path.resolve(output, filename);
+  var banner = "";
+  if (bundleType === "dev") {
+    banner = getBanner(version, filename);
+  }
+  if (fs.existsSync(output)) {
+    fs.unlinkSync(output);
+  }
+  await result.write(
+    getOutputOptions(output, name, bundleType, {
+      banner,
+      intro: intro(version)
+    })
+  );
 }
 module.exports = build;
